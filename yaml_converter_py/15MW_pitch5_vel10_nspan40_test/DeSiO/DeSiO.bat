86146e968af9243cbcd1b2889f5d2bd2e2e65b56
Jamal-dev <70589612+Jamal-dev@users.noreply.github.com>
2022-12-19 00:34:34 +0100

    Tested on Windows: Passed

--- yaml_converter_py/15MW_pitch5_vel10_nspan40_test/DeSiO/DeSiO.bat
+++ yaml_converter_py/15MW_pitch5_vel10_nspan40_test/DeSiO/DeSiO.bat
@@ -1,8 +1,8 @@
 set path_DeSiO=C:\Users\hente\Desktop\DeSiO_compile\DeSiO\DeSiO\x64\Release
 set path_mtee=DeSiO
 REM
-set path_fsi=/home/jamal/qt_project_learning/DeSIO_GUI/yaml_converter_py/15MW_pitch5_vel10_nspan40_test/DeSiO
-set path_struc=/home/jamal/qt_project_learning/DeSIO_GUI/yaml_converter_py/15MW_pitch5_vel10_nspan40_test/DeSiO-Structure
+set path_fsi=C:\repos\DeSIO_GUI\yaml_converter_py\15MW_pitch5_vel10_nspan40_test\DeSiO
+set path_struc=C:\repos\DeSIO_GUI\yaml_converter_py\15MW_pitch5_vel10_nspan40_test\DeSiO-Structure
 REM Start fsi simulation with DeSiO
 cd %path_fsi%
 REM Boost thread priority
