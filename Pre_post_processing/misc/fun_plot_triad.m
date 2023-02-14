4bebde50eb2049a756a5bb4fdceb75633afa12fa
Jamal-dev <jamal.bhatti@stud.uni-hannover.de>
2023-02-14 13:42:53 +0100

    converting_output_files folder is added. In this submodule output files will be refined to read them through paraview. read_input_file.ipynb file is added. This can read inputs from beaminput.txt file. Here it can not yet read the cross sectional properties.

--- Pre_post_processing/misc/fun_plot_triad.m
+++
@@ -1,5 +0,0 @@
-function hplot = plot_triad(dx,dy,dz,d,d_scale)
-dx = d_scale*dx; dy = d_scale*dy; dz = d_scale*dz;
-hplot(1) = plot3([d(1), d(1)+dx(1)],[d(2), d(2)+dx(2)],[d(3), d(3)+dx(3)],'-r','linewidth',2);
-hplot(2) = plot3([d(1), d(1)+dy(1)],[d(2), d(2)+dy(2)],[d(3), d(3)+dy(3)],'-g','linewidth',2);
-hplot(3) = plot3([d(1), d(1)+dz(1)],[d(2), d(2)+dz(2)],[d(3), d(3)+dz(3)],'-b','linewidth',2);
--- Pre_post_processing/misc/skew.m
+++
@@ -1,5 +0,0 @@
-function A = skew(a)
-A = [0, -a(3), a(2); 
-     a(3), 0, -a(1); 
-     -a(2) a(1), 0];
-return
